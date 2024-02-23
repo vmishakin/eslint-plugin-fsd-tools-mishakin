a5ee23d4e5c8d12f86bd7e03d848011ae35d6d77
Vadim Mishakin <vadmishakin@gmail.com>
2024-02-23 23:02:01 +0300

    autofix for path-checker rule

--- tests/lib/rules/path-checker.js
+++ tests/lib/rules/path-checker.js
@@ -37,11 +37,24 @@ ruleTester.run("path-checker", rule, {
       filename: '/Users/aboba/project/src/entities/Article/ui/Article/Article.tsx',
       code: "import {addCommentFormActions, addCommentFormReducer} from '@/entities/Article/model/slices/addComment'",
       errors: [{ message: "в рамках одного слайса пути должны быть относительными" }],
+      output: "import {addCommentFormActions, addCommentFormReducer} from '../../model/slices/addComment'"
     },
     {
       filename: '/Users/aboba/project/src/entities/Article/ui/Article/Article.tsx',
       code: "import {addCommentFormActions, addCommentFormReducer} from 'entities/Article/model/slices/addComment'",
       errors: [{ message: "в рамках одного слайса пути должны быть относительными" }],
+      output: "import {addCommentFormActions, addCommentFormReducer} from '../../model/slices/addComment'"
+    },
+    {
+      options: [
+        {
+          alias: '@'
+        }
+      ],
+      filename: '/Users/aboba/project/src/entities/Product/ui/Product/Product.tsx',
+      code: "import {ProductList} from '@/entities/Product/ui/ProductList/ProductList'",
+      errors: [{ message: "в рамках одного слайса пути должны быть относительными" }],
+      output: "import {ProductList} from '../ProductList/ProductList'"
     },
   ],
 });
